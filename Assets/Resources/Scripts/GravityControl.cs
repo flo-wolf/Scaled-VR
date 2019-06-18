0f847daf68799ad8ff337875fd4bb4764ff3617a
Florian Wolf <fw.flolf@gmail.com>
2019-06-18 13:33:01 +0200

    RoomBlockOut in new Scene, RopeTests

---
+++ Assets/Resources/Scripts/GravityControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityControl : MonoBehaviour
+{
+    Rigidbody rigid;
+
+    public Vector3 gravity = new Vector3 (0, 9.81f ,0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigid.velocity = rigid.velocity * 0 + gravity * Time.deltaTime;
+    }
+}
