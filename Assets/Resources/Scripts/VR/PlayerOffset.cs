55fc990cbb199178dae4a183e0588ee24a5fe1c9
Florian Wolf <fw.flolf@gmail.com>
2019-08-18 21:13:24 +0200

    Added offset script, to workaround VR space Y positioning problems on different devices. Change offset by pressing arrowkey up/down.

---
+++ Assets/Resources/Scripts/VR/PlayerOffset.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerOffset : MonoBehaviour
+{
+    [SerializeField] private float _offsetIncrement = 0.1f;
+
+    private float _offsetY;
+    private float _startY;
+
+    private float _offsetYmin = -4;
+    private float _offsetYmax = 4;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _startY = transform.position.y;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            _offsetY += _offsetIncrement;
+            _offsetY = Mathf.Clamp(_offsetY, _offsetYmin, _offsetYmax);
+            UpdateOffset();
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            _offsetY -= _offsetIncrement;
+            _offsetY = Mathf.Clamp(_offsetY, _offsetYmin, _offsetYmax);
+            UpdateOffset();
+        }
+    }
+
+    private void UpdateOffset()
+    {
+        transform.position = new Vector3(transform.position.x, _startY + _offsetY, transform.position.z);
+    }
+}
